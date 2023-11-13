bd28828a893a9d3de1d23800d76e50486159d98c
François Plet <fplet@cogelec.fr>
2023-11-13 11:17:28 +0100

    remove uncessary data

--- sql/init/3_data.sql
+++ sql/init/3_data.sql
@@ -1,7 +1,7 @@
 USE library;
 
 INSERT INTO users(id, email, password, type, is_active, token) VALUES
-(1, 'fplet@cogelec.fr', 'password','user', true, 'token');
+(1, 'fake-address@fake.fr', 'password','user', true, 'token');
 
 INSERT INTO authors(first_name, last_name, owner_id) VALUES
 ('Toto', 'Blue', 1),
